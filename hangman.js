5614b5ea08f16538b11db9070378b150024af70f
kyungjaee <kkyungjaee@gmail.com>
2025-10-20 10:33:57 -0400

    added guess letter functionality

--- hangman.js
+++ hangman.js
@@ -7,12 +7,32 @@ var word = "";
 function newGame() {
     var randomIndex = parseInt(Math.random()*POSSIBLE_WORDS.length);
     word = POSSIBLE_WORDS(randomIndex);
-
-    var clueString = "";
-    for(var i = 0;i<word.length;i++)
-    {
-        clueString+= " _ ";
-    }
+    guesses = "";
+   
+}
     var clue = document.getElementById("clue");
     clue.innerHTML = clueString; 
+
+    function guessLetter() { 
+        var input = document.getElementById("guess");
+        var letter = input.value;
+        guesses += letter;
+        updatePage();
 }
+
+function updatePage() {
+     var clueString = "";
+    for(var i = 0;i<word.length;i++) {
+        var currentLetter = word.charAt(i);
+    if (guesses.indexOf(currentLetter)>= 0)  {//you guessed it
+        clueString += currentLetter + " ";
+    }
+        else 
+            clueString+= " _ ";
+} 
+    var clue = document.getElementById("clue")
+    clue.innerHTML = clueString;
+
+    var guessArea = document.getElementById("guesses");
+    guessArea.innerHTML = "Guessed Letters:  " + guesses;
+}    
