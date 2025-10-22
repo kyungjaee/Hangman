70343707beeb733a9589035dc68cd00cfacfa3e1
kyungjaee <kkyungjaee@gmail.com>
2025-10-22 09:59:37 -0400

    hangman updates

--- hangman.js
+++ hangman.js
@@ -1,13 +1,16 @@
 var POSSIBLE_WORDS = ["obdurate", "verisimilitude", "defenestrate", "obsequious", "toady", "idempotent"];
-
-
+var MAX_GUESSES = 6;
 var word = "";
+var guesses = "";
+var guess_count = MAX_GUESSES;
 
 
 function newGame() {
     var randomIndex = parseInt(Math.random()*POSSIBLE_WORDS.length);
     word = POSSIBLE_WORDS(randomIndex);
     guesses = "";
+    guess_count = MAX_GUESSES;
+    updatePage();
    
 }
     var clue = document.getElementById("clue");
@@ -16,6 +19,9 @@ function newGame() {
     function guessLetter() { 
         var input = document.getElementById("guess");
         var letter = input.value;
+        if(word.indexOf(letter)< 0) {
+            guess_count--;
+        }
         guesses += letter;
         updatePage();
 }
@@ -32,7 +38,10 @@ function updatePage() {
 } 
     var clue = document.getElementById("clue")
     clue.innerHTML = clueString;
-
+    //
     var guessArea = document.getElementById("guesses");
     guessArea.innerHTML = "Guessed Letters:  " + guesses;
+
+    var image = document.getElementById("")
+    image.src = "images/hangman" + guess_count + ".gif";
 }    
