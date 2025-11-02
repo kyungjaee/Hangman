8e185e71d5ab24ca1498bb5b368886685f6adedf
kyungjaee <kkyungjaee@gmail.com>
2025-11-02 16:19:46 -0500

    Tried to make hangman perfect but it's alright

--- hangman.js
+++ hangman.js
@@ -1,47 +1,112 @@
+
 var POSSIBLE_WORDS = ["obdurate", "verisimilitude", "defenestrate", "obsequious", "toady", "idempotent"];
 var MAX_GUESSES = 6;
+
+
 var word = "";
 var guesses = "";
 var guess_count = MAX_GUESSES;
+var gameOver = false;
+
+
+function makeClue() {
+  var clueString = "";
+  for (var i = 0; i < word.length; i++) {
+    var currentLetter = word.charAt(i);
+    if (guesses.indexOf(currentLetter) >= 0) {
+      clueString += currentLetter + " ";
+    } else {
+      clueString += "_ ";
+    }
+  }
+  return clueString;
+}
+
+
+function updatePage(message) {
+  
+  var clue = document.getElementById("clue");
+  clue.textContent = makeClue();
+
+  
+  var guessArea = document.getElementById("guesses");
+  var guessedList = guesses ? "Guessed Letters: " + guesses.split("").join(", ") : "Guessed Letters: None yet";
+  guessArea.textContent = message ? (guessedList + " | " + message) : guessedList;
+
+  // I couldn't figure out why images wouldn't show up, had help with this part 
+  var image = document.querySelector("img");
+  if (image) {
+    image.src = "images/hangman" + guess_count + ".gif";
+    image.alt = "Hangman stage " + guess_count;
+  }
+}
 
 
 function newGame() {
-    var randomIndex = parseInt(Math.random()*POSSIBLE_WORDS.length);
-    word = POSSIBLE_WORDS(randomIndex);
-    guesses = "";
-    guess_count = MAX_GUESSES;
-    updatePage();
-   
-}
-    var clue = document.getElementById("clue");
-    clue.innerHTML = clueString; 
-
-    function guessLetter() { 
-        var input = document.getElementById("guess");
-        var letter = input.value;
-        if(word.indexOf(letter)< 0) {
-            guess_count--;
-        }
-        guesses += letter;
-        updatePage();
+  var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length); 
+  word = POSSIBLE_WORDS(randomIndex);
+  guesses = "";
+  guess_count = MAX_GUESSES;
+  updatePage();
 }
 
-function updatePage() {
-     var clueString = "";
-    for(var i = 0;i<word.length;i++) {
-        var currentLetter = word.charAt(i);
-    if (guesses.indexOf(currentLetter)>= 0)  {//you guessed it
-        clueString += currentLetter + " ";
-    }
-        else 
-            clueString+= " _ ";
-} 
-    var clue = document.getElementById("clue")
-    clue.innerHTML = clueString;
-    //
-    var guessArea = document.getElementById("guesses");
-    guessArea.innerHTML = "Guessed Letters:  " + guesses;
 
-    var image = document.getElementById("")
-    image.src = "images/hangman" + guess_count + ".gif";
-}    
+function guessLetter() {
+  var input = document.getElementById("guess");
+  var letter = (input.value || "").toLowerCase();
+  input.value = "";
+  input.focus();
+
+  
+  if (word === "") {
+    updatePage("Press New game first!");
+    return;
+  }
+
+  
+  if (gameOver) {
+    updatePage("Game is over. Press New game.");
+    return;
+  }
+
+  
+  if (letter.length !== 1 || letter < "a" || letter > "z") {
+    updatePage("Please enter one letter (A–Z).");
+    return;
+  }
+
+
+  if (guesses.indexOf(letter) >= 0) {
+    updatePage("You already guessed that letter.");
+    return;
+  }
+
+  
+  guesses += letter;
+
+  
+  if (word.indexOf(letter) < 0) {
+    guess_count = Math.max(0, guess_count - 1);
+  }
+
+
+  updatePage();
+
+  
+  if (makeClue().indexOf("_") === -1) {
+    gameOver = true;
+    document.getElementById("guess").disabled = true;
+    updatePage("You won! The word was " + word.toUpperCase() + ".");
+    return;
+  }
+
+  
+  if (guess_count <= 0) {
+    gameOver = true;
+    document.getElementById("guess").disabled = true;
+    updatePage("You lost! The word was " + word.toUpperCase() + ".");
+    return;
+  }
+};
+
+
